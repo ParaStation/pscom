20e1a2ab88a11975f881a385a85079eb3d30f02d
Jens Hauke <hauke@par-tec.com>
2014-11-20 16:30:42 +0100

    pscom4ofed: Fix handshake finalization.

--- lib/pscom4ofed/pscom_ofed.c
+++ lib/pscom4ofed/pscom_ofed.c
@@ -171,6 +171,8 @@ void pscom_ofed_init_con(pscom_con_t *con)
 
 	con->do_write = pscom_ofed_do_write;
 	con->close = pscom_ofed_con_close;
+
+	pscom_con_setup_ok(con);
 }
 
 /*********************************************************************/
