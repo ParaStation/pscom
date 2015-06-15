2b2b8752743ba33fbba8bd86c4841124f4244613
Jens Hauke <hauke@par-tec.com>
2015-06-15 10:53:24 +0200

    pscom_async: safe guard connection shut down (Send Good bye).

--- lib/pscom/pscom_con.c
+++ lib/pscom/pscom_con.c
@@ -653,18 +653,27 @@ void pscom_con_guard_error(void *_con) {
 	pscom_con_error(con, PSCOM_OP_RW, PSCOM_ERR_IOERROR);
 }
 
+#define GUARD_BYE 0x25
 
 static
-void pscom_guard_eof(ufd_t *ufd, ufd_info_t *ufd_info) {
+void pscom_guard_readable(ufd_t *ufd, ufd_info_t *ufd_info) {
 	pscom_con_t *con = (pscom_con_t *)ufd_info->priv;
+	char msg = 0;
+	int error = 0;
+
+	read(ufd_info->fd, &msg, 1); // Good bye or error?
+	error = msg != GUARD_BYE;
+
 	/* Callback called in the async thread!! */
-	DPRINT(0, "pscom guard con:%p terminated", con);
+	DPRINT(error ? 1 : 2, "pscom guard con:%p %s", con, error ? "terminated" : "closed");
 
 	// Stop listening
 	ufd_event_clr(ufd, ufd_info, POLLIN);
 
-	// Call pscom_con_guard_error in the main thread
-	pscom_backlog_push(pscom_con_guard_error, con);
+	if (error) {
+		// Call pscom_con_guard_error in the main thread
+		pscom_backlog_push(pscom_con_guard_error, con);
+	}
 }
 
 
@@ -679,7 +688,7 @@ void pscom_con_guard_start(pscom_con_t *con)
 	pre->closefd_on_cleanup = 0;
 	con->con_guard.fd = fd;
 	DPRINT(5, "precon(%p): Start guard on fd %d", pre, fd);
-	pscom_async_on_readable(fd, pscom_guard_eof, con);
+	pscom_async_on_readable(fd, pscom_guard_readable, con);
 }
 
 
@@ -687,8 +696,11 @@ void pscom_con_guard_stop(pscom_con_t *con)
 {
 	int fd = con->con_guard.fd;
 	if (fd != -1) {
+		char msg = GUARD_BYE;
+		write(con->con_guard.fd, &msg, 1); // Good bye
+
 		con->con_guard.fd = -1;
-		pscom_async_off_readable(fd, pscom_guard_eof, con);
+		pscom_async_off_readable(fd, pscom_guard_readable, con);
 		close(fd);
 	}
 	DPRINT(5, "Stop guard on fd %d", fd);
