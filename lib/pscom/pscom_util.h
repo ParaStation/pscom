eaeeb008188e0120c0a6769899ecb0dc7a2843ed
Jens Hauke <hauke@par-tec.com>
2015-10-05 14:44:34 +0200

    util: Moving likely/unlikely macros to pscom_util.h

--- lib/pscom/pscom_util.h
+++ lib/pscom/pscom_util.h
@@ -114,4 +114,17 @@ void pscom_memcpy_from_iov(char *data, const struct iovec *iov, size_t len)
 	}
 }
 
+
+/* Somewhere in the middle of the GCC 2.96 development cycle, we implemented
+   a mechanism by which the user can annotate likely branch directions and
+   expect the blocks to be reordered appropriately.  Define __builtin_expect
+   to nothing for earlier compilers.  */
+#if __GNUC__ == 2 && __GNUC_MINOR__ < 96
+#define __builtin_expect(x, expected_value) (x)
+#endif
+
+#define likely(x)	__builtin_expect((x),1)
+#define unlikely(x)	__builtin_expect((x),0)
+
+
 #endif /* _PSCOM_UTIL_H_ */
