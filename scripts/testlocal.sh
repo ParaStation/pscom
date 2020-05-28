6f0315a63a74acd7a3123c951be5f7ec92b608f9
Jens Hauke <hauke@par-tec.com>
2020-05-28 14:30:30 +0200

    test: testlocal: scan server output with read
    No sleep 1.
    Timeout 10sec, if server fails to produce the commandline output
    exit 1, if server fails

--- scripts/testlocal.sh
+++ scripts/testlocal.sh
@@ -1,18 +1,22 @@
-#!/bin/bash -x
+#!/bin/bash
 MSG_SIZE=$((32*1024))
-PSP_ENV="PSP_SHM=1 PSP_TCP=0"
+export PSP_SHM=2
+export PSP_TCP=0
+export PSP_DEBUG=3
+
+# Return with an exit code if the pscom_pp server fails
+set -e -o pipefail
 
 # Run the server
-CLIENT_COMMAND_FILE=$(mktemp)
-env ${PSP_ENV} ./pscom_pp -1 -V &> ${CLIENT_COMMAND_FILE} &
-sleep 1
+../bin/pscom_pp -1 -V | while read -t 10 cmd args; do
+	case $cmd in
+		# Run the client with arguments we got from the server:
+		# > Waiting for client.
+		# > Call client with:
+		# > ./pscom_pp -c 192.168.1.1:7100
 
-# Exit if the server could not be started
-kill -0 $! || exit 1
-
-# Retrieve client command
-CLIENT_COMMAND=$( tail -n 1 ${CLIENT_COMMAND_FILE} )
-rm ${CLIENT_COMMAND_FILE}
-
-# Run the client
-bash -c "env ${PSP_ENV} ${CLIENT_COMMAND} --minsize=${MSG_SIZE} --maxsize=${MSG_SIZE} -V"
+		*pscom_pp)
+			$cmd $args --minsize=${MSG_SIZE} --maxsize=${MSG_SIZE} -V
+			;;
+	esac
+done
