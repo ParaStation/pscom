b04821e445fba9655a2b1c35b733f2db909a0adb
Simon Pickartz <pickartz@par-tec.com>
2020-06-23 06:19:03 +0200

    cmake: Add hints to find EXTOLL libraries.

--- cmake/Modules/FindExtoll.cmake
+++ cmake/Modules/FindExtoll.cmake
@@ -1,7 +1,18 @@
 
-find_path(EXTOLL_INCLUDE_DIR rma2.h velo2.h)
-find_library(EXTOLL_RMA2_LIBRARY rma2)
-find_library(EXTOLL_VELO2_LIBRARY velo2)
+set(EXTOLL_HOME "/opt/extoll/x86_64" CACHE STRING "Default location where to search the EXTOLL libraries.")
+
+find_path(EXTOLL_INCLUDE_DIR
+  NAMES rma2.h velo2.h
+  HINTS ${EXTOLL_HOME}
+  PATH_SUFFIXES include)
+find_library(EXTOLL_RMA2_LIBRARY
+  NAMES rma2
+  HINTS ${EXTOLL_HOME}
+  PATH_SUFFIXES lib lib64)
+find_library(EXTOLL_VELO2_LIBRARY
+  NAMES velo2
+  HINTS ${EXTOLL_HOME}
+  PATH_SUFFIXES lib lib64)
 
 set(EXTOLL_LIBRARIES ${EXTOLL_RMA2_LIBRARY} ${EXTOLL_VELO2_LIBRARY})
 set(EXTOLL_INCLUDE_DIRS ${EXTOLL_INCLUDE_DIR})
