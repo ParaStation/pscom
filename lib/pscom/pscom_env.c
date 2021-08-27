3fa9bcf86c6267b8cb4204965a8d828773d1ff9b
Simon Pickartz <pickartz@par-tec.com>
2021-08-27 17:16:40 +0200

    env: Use pscom_env_table for setting environment parameters.
    Switch to the new table-based infrastructure for reading the environment
    variables instead of reading them individually.

--- lib/pscom/pscom_env.c
+++ lib/pscom/pscom_env.c
@@ -572,51 +572,27 @@ void pscom_env_init(void)
 {
 	pscom_pslib_read_config(ENV_CONFIG_FILES);
 
-	pscom_debug_set_filename(pscom_env_get(ENV_DEBUG_OUT), 1);
-	if (pscom.env.debug == -1) { // only set if pscom_set_debug() was not called before.
-		pscom.env.debug = D_ERR; // Default: show errors.
-		pscom_env_get_int(&pscom.env.debug, ENV_DEBUG);
-	}
+	pscom_env_table_register_and_parse("pscom general", NULL,
+					   pscom_env_table);
+	pscom_debug_set_filename(pscom.env.debug_out, 1);
 
-	pscom_env_get_str(&pscom.env.debug_timing, ENV_DEBUG_TIMING);
 	pscom_dtime_init();
 
 	if (pscom_pslib_available) {
 		pscom_env_get_str(&pscom.env.info, ENV_INFO);
 		if (pscom.env.info) pscom_info_connect(pscom.env.info);
 	}
-	pscom_env_get_int(&pscom.env.debug_version, ENV_DEBUG_VERSION);
 
 	DPRINT(D_VERSION, "# Version(PSCOM): %s (%s)" PSCOM_IF_CUDA("+cuda", "")
 #ifdef PSCOM_ALLIN
 	       "+allin"
 #endif
 	       ,__DATE__, VC_VERSION);
-	pscom_env_get_uint(&pscom.env.so_sndbuf, ENV_SO_SNDBUF);
-	pscom_env_get_uint(&pscom.env.so_rcvbuf, ENV_SO_RCVBUF);
-	pscom_env_get_int(&pscom.env.tcp_nodelay, ENV_TCP_NODELAY);
-	pscom_env_get_uint(&pscom.env.tcp_backlog, ENV_TCP_BACKLOG);
 
-	// pscom_env_get_int(&env.nobgthread, ENV_NOBGTHREAD);
-	pscom_env_get_int(&pscom.env.sched_yield, ENV_SCHED_YIELD);
-	pscom_env_get_int(&pscom.env.unexpected_receives, ENV_UNEXPECTED_RECEIVES);
-	pscom_env_get_uint(&pscom.env.rendezvous_size, ENV_RENDEZVOUS);
 
-	pscom_env_get_uint(&pscom.env.ucp_max_recv, ENV_UCP_MAX_RECV);
 
 #ifdef PSCOM_CUDA_AWARENESS
-	pscom_env_get_int(&pscom.env.cuda, ENV_CUDA);
-
-	pscom_env_get_uint(&pscom.env.cuda_sync_memops, ENV_CUDA_SYNC_MEMOPS);
-
-	pscom_env_get_uint(&pscom.env.cuda_aware_shm, ENV_CUDA_AWARE_SHM);
-	pscom_env_get_uint(&pscom.env.cuda_aware_openib, ENV_CUDA_AWARE_OPENIB);
-	pscom_env_get_uint(&pscom.env.cuda_aware_ucp, ENV_CUDA_AWARE_UCP);
-	pscom_env_get_uint(&pscom.env.cuda_aware_velo, ENV_CUDA_AWARE_VELO);
-	pscom_env_get_uint(&pscom.env.cuda_aware_extoll, ENV_CUDA_AWARE_EXTOLL);
-
 	/* one environment variable disabling CUDA-awareness of all plugins */
-	pscom_env_get_uint(&pscom.env.cuda_enforce_staging, ENV_CUDA_ENFORCE_STAGING);
 	if (pscom.env.cuda_enforce_staging == 1) {
 		pscom.env.cuda_aware_shm    = 0;
 		pscom.env.cuda_aware_openib = 0;
@@ -626,6 +602,10 @@ void pscom_env_init(void)
 	}
 #endif
 
+	/* TODO:
+	 * Move pluging-specific configuration parameters to the respective
+	 * plugins. Then we can avoid reading these environment variables again
+	 */
 	if (pscom.env.rendezvous_size != (unsigned)~0)
 		pscom.env.rendezvous_size_shm = pscom.env.rendezvous_size;
 	pscom_env_get_uint(&pscom.env.rendezvous_size_shm, ENV_RENDEZVOUS_SHM);
@@ -654,28 +634,9 @@ void pscom_env_init(void)
 		pscom.env.rendezvous_size_ucp = pscom.env.rendezvous_size;
 	pscom_env_get_uint(&pscom.env.rendezvous_size_ucp, ENV_RENDEZVOUS_UCP);
 
-	pscom_env_get_int(&pscom.env.sigquit, ENV_SIGQUIT);
-	pscom_env_get_int(&pscom.env.sigsuspend, ENV_SIGSUSPEND);
-	pscom_env_get_int(&pscom.env.sigsegv, ENV_SIGSEGV);
-	pscom_env_get_uint(&pscom.env.readahead, ENV_READAHEAD);
-	pscom_env_get_uint(&pscom.env.retry, ENV_RETRY);
-	pscom_env_get_uint(&pscom.env.precon_reconnect_timeout, ENV_RECONNECT_TIMEOUT);
-	pscom_env_get_uint(&pscom.env.precon_connect_stalled_max, ENV_CONNECT_STALLED_MAX);
-
-	pscom.env.readahead = pscom_max(pscom.env.readahead, (unsigned)sizeof(pscom_header_net_t));
-
-	pscom_env_get_uint(&pscom.env.guard, ENV_GUARD);
-
-	pscom_env_get_str(&pscom.env.network, ENV_NETWORK);
-	pscom_env_get_dir(&pscom.env.plugindir, ENV_PLUGINDIR);
-
-	pscom_env_get_int(&pscom.env.debug_req, ENV_DEBUG_REQ);
-	pscom_env_get_int(&pscom.env.debug_stats, ENV_DEBUG_STATS);
-	pscom_env_get_int(&pscom.env.debug_contype, ENV_DEBUG_CONTYPE);
-	pscom_env_get_int(&pscom.env.debug_suspend, ENV_DEBUG_SUSPEND);
-	pscom_env_get_int(&pscom.env.debug_precon, ENV_DEBUG_PRECON);
-
-	pscom_env_get_uint(&pscom.env.iprobe_count, ENV_IPROBE_COUNT);
+	/* the readahead buffer has to store the pscom_header_net  at least*/
+	pscom.env.readahead = pscom_max(pscom.env.readahead,
+					(unsigned)sizeof(pscom_header_net_t));
 }
 
 
