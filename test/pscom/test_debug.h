04aa4e5d44156fc672f224803d8b11c8cd3f987c
Simon Pickartz <pickartz@par-tec.com>
2022-02-18 12:42:36 +0100

    utest/debug: Add test for PSP_DEBUG_OUT.
    This test checks whether PSP_DEBUG_OUT ensures that nothing is written
    to stderr if `PSP_DEBUG_OUT` is set.

---
+++ test/pscom/test_debug.h
@@ -0,0 +1,16 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2021-2022 ParTec AG, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ */
+
+#ifndef _TEST_DEBUG_H_
+#define _TEST_DEBUG_H_
+
+void test_debug_psp_debug_out_max_debug_level(void **state);
+
+#endif /* _TEST_DEBUG_H_ */
