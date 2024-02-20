00f7dd5fd68bfd047b290f303a792c8490ed51e0
Simon Pickartz <pickartz@par-tec.com>
2024-02-20 16:57:10 +0100

    cmake: Use new helper to find packages using hints.
    This patch leverages the new CMake helper function to find packages by
    taking provided hints into account. This affects the following packages:
    
    * CUDA
    * DAPL
    * IB Verbs
    * popt
    * Portals4
    * PSM2
    * UCX

---
+++ cmake/Modules/AddCudaTarget.cmake
@@ -0,0 +1,11 @@
+function(target_add_cuda target)
+  target_include_directories(
+    ${target}
+    PRIVATE ${CUDA_INCLUDE_DIRS}
+    )
+
+  target_link_libraries(
+    ${target}
+    PRIVATE ${CUDA_LIBRARIES}
+    )
+endfunction()
