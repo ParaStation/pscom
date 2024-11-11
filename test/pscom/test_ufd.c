164edaa20b911510a8dbe6db015f86ae104d71a3
Carsten Clauss <clauss@par-tec.com>
2024-11-11 13:32:42 +0100

    utest/ufd: Add tests for init/clean-up and updates in threaded mode.
    This commit adds two tests for the general initialization of the ufd
    data structures and their proper cleanup at the end, as well as two
    tests that check for correct handling of allowed/not-allowed accesses
    to ufd array elements in the threaded case with respect to updates of
    the global ufd array.

--- test/pscom/test_ufd.c
+++ test/pscom/test_ufd.c
@@ -18,6 +18,7 @@
 #include <errno.h>
 #include <poll.h>
 
+#include "list.h"
 #include "mocks/misc_mocks.h"
 #include "pscom_precon.h"
 #include "pscom_priv.h"
@@ -57,6 +58,176 @@ int mock_sched_yield_close_global_ufd(void *arg)
 
     return 0;
 }
+
+/* Global ufd info object that can be accessed by callback functions below */
+static ufd_info_t global_ufd_info;
+
+static void remove_global_pollfd_info_from_ufd_array(ufd_t *ufd,
+                                                     ufd_info_t *ufd_info)
+{
+    /* remove the global_ufd_info object from the ufd array */
+    _ufd_put_pollfd_idx(ufd, &global_ufd_info);
+    function_called();
+}
+
+static void add_global_pollfd_info_to_ufd_array(ufd_t *ufd, ufd_info_t *ufd_info)
+{
+    /* add the global_ufd_info object to the ufd array */
+    _ufd_get_pollfd_idx(ufd, &global_ufd_info);
+
+    /* and (re)set its function pointers to the common mock
+       functions that check if can_read/write are called */
+    global_ufd_info.can_read  = &check_can_read;
+    global_ufd_info.can_write = &check_can_write;
+
+    function_called();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+/// The ufd tests
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * \brief Test the general ufd initialization and cleaning-up
+ *
+ * Given: The global ufd object and a TCP precon
+ * When: the file descriptor of the precon is set for being polled
+ * for different events
+ * Then: all ufd related data structures are being initialized
+ * correctly and properly cleaned-up at the end
+ */
+void test_ufd_init_set_and_cleanup(void **state)
+{
+    /* initialize and do checks with global ufd object in non-threaded mode */
+    ufd_t *ufd = test_utils_init_ufd(&pscom.ufd);
+
+    /* make sure that ufd has been initialized correctly */
+    assert_true((&ufd->ufd_info)->next == &ufd->ufd_info);
+    assert_true((&ufd->ufd_info)->prev == &ufd->ufd_info);
+    assert_true(ufd->n_ufd_pollfd == 0);
+    assert_true(ufd->ufd_tag == NULL);
+
+    /* create and initialize the precon object */
+    pscom_precon_t *precon      = (pscom_precon_t *)(*state);
+    pscom_precon_tcp_t *pre_tcp = (pscom_precon_tcp_t *)&precon->precon_data;
+    pscom_precon_assign_fd_tcp(pre_tcp, 0x42);
+
+    /* check that ufd_info has been initialized correctly */
+    assert_true((&pre_tcp->ufd_info)->pollfd_idx == -1);
+    assert_true((&pre_tcp->ufd_info)->fd == 0x42);
+
+    /* check that ufd_info has been added to the global list */
+    ufd_info_t *ufd_info = ufd_info_find_fd(ufd, 0x42);
+    assert_true(ufd_info == &pre_tcp->ufd_info);
+
+    /* check again that the ufd array is still empty */
+    assert_true(ufd->n_ufd_pollfd == 0);
+
+    /* set POLLIN event */
+    ufd_event_set(ufd, &pre_tcp->ufd_info, POLLIN);
+
+    /* check that pollfd has been added */
+    assert_true(ufd->n_ufd_pollfd == 1);
+    assert_true((&pre_tcp->ufd_info)->pollfd_idx >= 0);
+
+    /* check that pollfd has been set correctly */
+    struct pollfd *pollfd = ufd_get_pollfd(ufd, &pre_tcp->ufd_info);
+    assert_true(pollfd != NULL);
+    assert_true(pollfd->fd == 0x42);
+    assert_true(pollfd->events == POLLIN);
+    assert_true(pollfd->revents == 0);
+
+    /* set POLLOUT event in addition and store index before */
+    int old_idx = (&pre_tcp->ufd_info)->pollfd_idx;
+    ufd_event_set(ufd, &pre_tcp->ufd_info, POLLOUT);
+
+    /* check that event has been set and index hasn't changed */
+    assert_true(pollfd->events == (POLLIN | POLLOUT));
+    assert_true((&pre_tcp->ufd_info)->pollfd_idx == old_idx);
+
+    /* clear each of both events and check it */
+    ufd_event_clr(ufd, &pre_tcp->ufd_info, POLLIN);
+    ufd_event_clr(ufd, &pre_tcp->ufd_info, POLLOUT);
+    assert_true(pollfd->events == 0);
+
+    /* check also that pollfd/ufd_info has been removed */
+    assert_true(ufd->n_ufd_pollfd == 0);
+    assert_true((&pre_tcp->ufd_info)->pollfd_idx == -1);
+    pollfd = ufd_get_pollfd(ufd, &pre_tcp->ufd_info);
+    assert_true(pollfd == NULL);
+
+    /* clean up the ufd */
+    test_utils_cleanup_ufd(ufd);
+}
+
+
+/**
+ * \brief Test ufd initialization and cleaning-up in the threaded case
+ *
+ * Given: The global ufd object and a TCP precon in a multi-threaded
+ * environment
+ * When: the file descriptor of the precon is set for being polled
+ * for different events
+ * Then: all ufd related data structures (especially also the ufd_tag for
+ * thread synchronization) are being set correctly as well as cleaned-up
+ * at the end
+ */
+void test_ufd_init_set_and_cleanup_threaded(void **state)
+{
+    /* initialize and do checks with the global ufd object in threaded mode */
+    ufd_t *ufd = test_utils_init_ufd_threaded(&pscom.ufd);
+
+    /* make sure that the ufd tag array has been allocated */
+    assert_true(ufd->ufd_tag != NULL);
+
+    /* create and initialize the precon object */
+    pscom_precon_t *precon      = (pscom_precon_t *)(*state);
+    pscom_precon_tcp_t *pre_tcp = (pscom_precon_tcp_t *)&precon->precon_data;
+    pscom_precon_assign_fd_tcp(pre_tcp, 0x42);
+
+    /* set POLLIN event */
+    ufd_event_set(ufd, &pre_tcp->ufd_info, POLLIN);
+
+    /* check that events have been added to array */
+    int idx = (&pre_tcp->ufd_info)->pollfd_idx;
+    assert_true(idx >= 0);
+
+    /* check the tag for this pollfd entry */
+    uint64_t tag = ufd->ufd_tag[idx];
+    assert_true(tag > 0);
+
+    /* set POLLOUT event in addition */
+    ufd_event_set(ufd, &pre_tcp->ufd_info, POLLOUT);
+
+    /* check that tag has been updated */
+    assert_true(ufd->ufd_tag[idx] != tag);
+
+    /* clear POLLIN event */
+    tag = ufd->ufd_tag[idx];
+    ufd_event_clr(ufd, &pre_tcp->ufd_info, POLLIN);
+
+    /* check that tag has been updated */
+    assert_true(ufd->ufd_tag[idx] != tag);
+
+    /* now clear also POLLOUT event */
+    tag = ufd->ufd_tag[idx];
+    ufd_event_clr(ufd, &pre_tcp->ufd_info, POLLOUT);
+
+    /* check that tag has been updated */
+    assert_true(ufd->ufd_tag[idx] != tag);
+
+    /* and check that the entry has been removed */
+    idx = (&pre_tcp->ufd_info)->pollfd_idx;
+    assert_true(idx == -1);
+
+    /* clean up ufd and disable threaded mode */
+    test_utils_cleanup_ufd_threaded(ufd);
+
+    /* check that the ufd tag array has been released */
+    assert_true(ufd->ufd_tag == NULL);
+}
+
 
 /**
  * \brief Test avoid writing when preconnection was refused
@@ -474,6 +645,162 @@ void test_ufd_do_not_write_if_global_ufd_is_gone(void **state)
     /* implicit test: check_can_write() is NOT called */
 
     disable_sched_yield_mock();
+
+    /* clean up ufd and disable threaded mode */
+    test_utils_cleanup_ufd_threaded(ufd);
+}
+
+
+/**
+ * \brief Test that a UFD array element is not processed if it has been updated
+ *
+ * Given: A multi-threaded environment
+ * When: updating the global pollfd array while reading/writing
+ * Then: an array element that has been changed will not be processed for read
+ * since the thread-local copy has become outdated
+ */
+void test_ufd_do_not_read_when_array_is_updated(void **state)
+{
+    /* initialize and do checks with the global ufd object in threaded mode */
+    ufd_t *ufd = test_utils_init_ufd_threaded(&pscom.ufd);
+
+    /* create and initialize the precon object */
+    pscom_precon_t *precon      = (pscom_precon_t *)(*state);
+    pscom_precon_tcp_t *pre_tcp = (pscom_precon_tcp_t *)&precon->precon_data;
+    pscom_precon_assign_fd_tcp(pre_tcp, 0x42);
+
+    /* This test adds two ufd_info objects to the global ufd array:
+     * (1) pre_tcp->ufd_info (the ufd_info attached to the precon object)
+     * (2) global_ufd_info (the ufd object accessible by the callback functions)
+     */
+
+    /* (1) set the ufd info of the precon */
+    ufd_event_set(&pscom.ufd, &pre_tcp->ufd_info, POLLIN | POLLOUT);
+    /* check that it has been added at the first position of the array */
+    assert_true(pscom.ufd.ufd_pollfd_info[0] == &pre_tcp->ufd_info);
+    /* set can_read() so that the global info object is removed from array */
+    pre_tcp->ufd_info.can_read = &remove_global_pollfd_info_from_ufd_array;
+    expect_function_call_any(remove_global_pollfd_info_from_ufd_array);
+    /* set can_write() so that the global ufd info is added again to array */
+    pre_tcp->ufd_info.can_write = &add_global_pollfd_info_to_ufd_array;
+    expect_function_call_any(add_global_pollfd_info_to_ufd_array);
+
+    /* (2) add and set the global ufd info object */
+    ufd_add(&pscom.ufd, &global_ufd_info);
+    ufd_event_set(&pscom.ufd, &global_ufd_info, POLLIN);
+    /* check that it has been added at the second position of the array */
+    assert_true(pscom.ufd.ufd_pollfd_info[1] == &global_ufd_info);
+    /* implicit test: check_can_read() is NOT called */
+    global_ufd_info.can_read = &check_can_read;
+
+    /* let poll report POLLIN | POLLOUT for both fds */
+    will_return(__wrap_poll, (POLLIN | POLLOUT));
+    will_return(__wrap_poll, 2);
+
+    /* Do the ufd processing: While the first object is being processed,
+     * `can_read()` removes the second object, whereas the subsequent
+     * `can_write()` adds it back again. However, when the second object
+     * is then processed afterwards, `can_read()` must not be called here
+     * because even if the pointer hasn't changed, the array has been updated.
+     */
+    pscom_progress(0);
+
+    /* implicit test: check_can_read() is NOT called */
+
+    /* check that the order of the two objects in the array is still the same */
+    assert_true(pscom.ufd.ufd_pollfd_info[0] == &pre_tcp->ufd_info);
+    assert_true(pscom.ufd.ufd_pollfd_info[1] == &global_ufd_info);
+
+    /* clean up ufd and disable threaded mode */
+    test_utils_cleanup_ufd_threaded(ufd);
+}
+
+
+/**
+ * \brief Test that a UFD array element is not processed if it has been updated
+ *
+ * Given: A multi-threaded environment
+ * When: updating the global pollfd array while reading/writing
+ * Then: an array element that has been changed will not be processed for write
+ * since the thread-local copy has become outdated
+ */
+void test_ufd_do_not_write_when_array_is_updated(void **state)
+{
+    /* initialize and do checks with the global ufd object in threaded mode */
+    ufd_t *ufd = test_utils_init_ufd_threaded(&pscom.ufd);
+
+    /* create and initialize the precon object */
+    pscom_precon_t *precon      = (pscom_precon_t *)(*state);
+    pscom_precon_tcp_t *pre_tcp = (pscom_precon_tcp_t *)&precon->precon_data;
+    pscom_precon_assign_fd_tcp(pre_tcp, 0x42);
+
+    /* This test adds three ufd_info objects to the global ufd array:
+     * (1) global_ufd_info (the ufd object accessible by the callback functions)
+     * (2) pre_tcp->ufd_info (the ufd_info attached to the precon object)
+     * (3) ufd_info_dummy (a function-local ufd info object as a dummy)
+     */
+
+    ufd_info_t ufd_info_dummy;
+
+    /* (1) add and set the global ufd info object */
+    ufd_add(&pscom.ufd, &global_ufd_info);
+    ufd_event_set(&pscom.ufd, &global_ufd_info, POLLIN | POLLOUT);
+    /* check that it has been added at the beginning of the array */
+    assert_true(pscom.ufd.ufd_pollfd_info[0] == &global_ufd_info);
+    /* set can_read() so that this object is removed again from the array */
+    global_ufd_info.can_read = &remove_global_pollfd_info_from_ufd_array;
+    expect_function_call_any(remove_global_pollfd_info_from_ufd_array);
+    /* implicit test: check_can_write() is NOT called */
+    global_ufd_info.can_write = &check_can_write;
+
+    /* (2) set the ufd info of the precon */
+    ufd_event_set(&pscom.ufd, &pre_tcp->ufd_info, POLLIN | POLLOUT);
+    /* check that it has been added at the second position of the array */
+    assert_true(pscom.ufd.ufd_pollfd_info[1] == &pre_tcp->ufd_info);
+    /* check that can_read() is still being called */
+    pre_tcp->ufd_info.can_read = &check_can_read;
+    expect_function_call_any(check_can_read);
+    /* set can_write() so that the global ufd info is added again */
+    pre_tcp->ufd_info.can_write = &add_global_pollfd_info_to_ufd_array;
+    expect_function_call_any(add_global_pollfd_info_to_ufd_array);
+
+    /* (3) add and set the dummy info object */
+    ufd_add(&pscom.ufd, &ufd_info_dummy);
+    /* check for POLLOUT only to see if object is being processed */
+    ufd_event_set(&pscom.ufd, &ufd_info_dummy, POLLOUT);
+    /* check that it has been added at the third position */
+    assert_true(pscom.ufd.ufd_pollfd_info[2] == &ufd_info_dummy);
+    /* implicit test: check_can_write() is NOT called */
+    ufd_info_dummy.can_write = &check_can_write;
+
+    /* let poll report POLLIN | POLLOUT for all three fds */
+    will_return(__wrap_poll, (POLLIN | POLLOUT));
+    will_return(__wrap_poll, 3);
+
+    /* check again that the array is set up correctly */
+    assert_true(pscom.ufd.n_ufd_pollfd == 3);
+    assert_true(pscom.ufd.ufd_pollfd_info[0] == &global_ufd_info);
+    assert_true(pscom.ufd.ufd_pollfd_info[1] == &pre_tcp->ufd_info);
+    assert_true(pscom.ufd.ufd_pollfd_info[2] == &ufd_info_dummy);
+
+    /* Do the ufd processing: While `can_read()` is called on the first
+     * ufd info object in the array (index 0), this object gets removed
+     * from the array and the last of the three elements is moved to this
+     * position instead. When the second element is then processed afterwards,
+     * the removed object is added back by this `can_read()` method -- now
+     * becoming the object at the third position in the array. However, during
+     * processing of this third element, the `can_write()` operation must not
+     * be called because it has been moved and the array has been updated.
+     */
+    pscom_progress(0);
+
+    /* implicit test: check_can_write() is NOT called */
+    /* because the array entry/index of global_pollfd_info has been changed */
+
+    /* check that the objects are located in the array in the right order */
+    assert_true(pscom.ufd.ufd_pollfd_info[0] == &ufd_info_dummy);
+    assert_true(pscom.ufd.ufd_pollfd_info[1] == &pre_tcp->ufd_info);
+    assert_true(pscom.ufd.ufd_pollfd_info[2] == &global_ufd_info);
 
     /* clean up ufd and disable threaded mode */
     test_utils_cleanup_ufd_threaded(ufd);
--- test/pscom/test_ufd.h
+++ test/pscom/test_ufd.h
@@ -11,6 +11,8 @@
 #ifndef _TEST_UFD_H_
 #define _TEST_UFD_H_
 
+void test_ufd_init_set_and_cleanup(void **state);
+void test_ufd_init_set_and_cleanup_threaded(void **state);
 void test_ufd_do_not_write_when_con_refused(void **state);
 void test_ufd_do_not_write_con_reset_by_peer(void **state);
 void test_ufd_do_not_write_when_pollfd_is_cleared(void **state);
@@ -21,5 +23,7 @@ void test_ufd_read_and_write_normally(void **state);
 void test_ufd_only_write_when_no_pollin(void **state);
 void test_ufd_do_not_read_if_global_ufd_is_gone(void **state);
 void test_ufd_do_not_write_if_global_ufd_is_gone(void **state);
+void test_ufd_do_not_read_when_array_is_updated(void **state);
+void test_ufd_do_not_write_when_array_is_updated(void **state);
 
 #endif /* _TEST_UFD_H_ */
