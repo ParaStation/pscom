b265ad3396c035ec5330dc040939d68de985b1f7
Simon Pickartz <pickartz@par-tec.com>
2025-01-13 15:55:52 +0100

    copyright: Update the copyright headers.

--- lib/pscom/perf.h
+++ lib/pscom/perf.h
@@ -1,3 +1,14 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2007-2021 ParTec Cluster Competence Center GmbH, Munich
+ * Copyright (C) 2021-2025 ParTec AG, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ */
+
 #ifndef _PERF_H_
 #define _PERF_H_
 
