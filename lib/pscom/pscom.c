0e3c5ecffa12b293e218946be4f5d0d3fcbd45b1
Simon Pickartz <pickartz@par-tec.com>
2025-03-25 10:56:10 +0100

    version: Move version handling into separate module.

--- lib/pscom/pscom.c
+++ lib/pscom/pscom.c
@@ -37,6 +37,7 @@
 #include "pscom_precon.h"
 #include "pscom_queues.h"
 #include "pscom_ufd.h"
+#include "pscom_version.h"
 #include "pslib.h"
 
 #ifdef PSCOM_CUDA_AWARENESS
@@ -315,30 +316,23 @@ PSCOM_API_EXPORT
 pscom_err_t pscom_init(int pscom_version)
 {
     static int init = 1;
+    pscom_err_t err;
 
     perf_add("init");
 
-    int cuda = (pscom_version >> PSCOM_ABI_VERSION_CUDA_SHIFT) &
-               PSCOM_BIT_MASK(PSCOM_ABI_VERSION_CUDA_BITS);
-    int major = (pscom_version >> PSCOM_ABI_VERSION_MAJOR_SHIFT) &
-                PSCOM_BIT_MASK(PSCOM_ABI_VERSION_MAJOR_BITS);
-    int minor = pscom_version & PSCOM_BIT_MASK(PSCOM_ABI_VERSION_MINOR_BITS);
+    int cuda, major, minor;
+    PSCOM_VERSION_SPLIT(pscom_version, cuda, major, minor);
 
-    if (!(PSCOM_ABI_CUDA_SUPPORT) && cuda) {
-        DPRINT(D_FATAL, "Error: libpscom: Application defined "
-                        "PSCOM_CUDA_AWARENESS, but this libpscom is build "
-                        "without.");
-        return PSCOM_ERR_UNSUPPORTED_VERSION;
-    }
-    if ((major != PSCOM_ABI_VERSION_MAJOR) ||
-        (minor > PSCOM_ABI_VERSION_MINOR)) {
-        // different major number, or minor number bigger
-        // (new libs support old api, if major number is equal)
+    if ((err = pscom_version_check(pscom_version, PSCOM_VERSION)) !=
+        PSCOM_SUCCESS) {
         DPRINT(D_FATAL,
                "Error: libpscom ABI version mismatch! Application requested "
-               "V%u.%u but libpscom is V%u.%u.",
-               major, minor, PSCOM_ABI_VERSION_MAJOR, PSCOM_ABI_VERSION_MINOR);
-        return PSCOM_ERR_UNSUPPORTED_VERSION;
+               "V%u.%u%s but libpscom is V%u.%u%s.",
+               major, minor, cuda ? "(+cuda)" : "", PSCOM_ABI_VERSION_MAJOR,
+               PSCOM_ABI_VERSION_MINOR,
+               PSCOM_ABI_CUDA_SUPPORT ? "(+cuda)" : "");
+
+        return err;
     }
 
     if (init <= 0) { goto out; }
---
+++ lib/pscom/pscom_version.h
@@ -0,0 +1,8 @@
+#ifndef _PSCOM_VERSION_H_
+#define _PSCOM_VERSION_H_
+
+#include "pscom.h"
+
+pscom_err_t pscom_version_check(int pscom_version_required, int pscom_version);
+
+#endif /* _PSCOM_VERSION_H_ */
