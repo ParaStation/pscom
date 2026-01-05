e1af732337bbbb894bd1ad842063f5449db4213d
Simon Pickartz <pickartz@par-tec.com>
2026-01-05 15:47:57 +0100

    copyright: Update the copyright headers.

--- lib/pscom/pscom_version.h
+++ lib/pscom/pscom_version.h
@@ -1,3 +1,12 @@
+/*
+ * ParaStation
+ *
+ * Copyright (C) 2025-2026 ParTec AG, Munich
+ *
+ * This file may be distributed under the terms of the Q Public License
+ * as defined in the file LICENSE.QPL included in the packaging of this
+ * file.
+ */
 #ifndef _PSCOM_VERSION_H_
 #define _PSCOM_VERSION_H_
 
